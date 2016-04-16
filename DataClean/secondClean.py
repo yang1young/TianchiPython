ff0006b3e52083edaafec61627e3526954a41208
HaizhenZhuo <zhuohaizhen@gmail.com>
2016-04-16 11:44:41 +0800

    add less and more to 2_isf2.csv

--- DataClean/secondClean.py
+++ DataClean/secondClean.py
@@ -15,17 +15,22 @@ isf1 = open("/Users/zhuohaizhen/PycharmProjects/Tianchi_Python/Data/OutputData/1
 isf2 = open("/Users/zhuohaizhen/PycharmProjects/Tianchi_Python/Data/OutputData/2_isf2.csv",'wb+')
 config1 = open("/Users/zhuohaizhen/PycharmProjects/Tianchi_Python/Data/OutputData/1_config1.csv",'r')
 
-
+i = 0
 for line in config1.readlines():
     list1 = line.strip().split(',')
     key = str(list1[1])+str(list1[2])
     config_dic[key] = list1[3]
 
 for line in isf1.readlines():
+    if i==0:
+        line = line.strip() + ",less,more"
+        i = 1
+        isf2.writelines(line+'\n')
+        continue
     list1 = line.strip().split(',')
     key  = str(list1[2])+str(list1[3])
     a_b = config_dic[key]
-    line = line.strip() + ","+config_dic[key]
+    line = line.strip() + ","+config_dic[key].split('_')[0]+"," + config_dic[key].split('_')[1]
     isf2.writelines(line+'\n')
     #print config_dic[key]
 
